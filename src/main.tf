51aa8d6ea3cb3f3fbc4b2709323eb8226943eac3
mclacore <michael@massdriver.cloud>
2022-08-25 14:41:15 -0600

    Adding tags for compliance

--- src/main.tf
+++ src/main.tf
@@ -8,6 +8,8 @@ locals {
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.vnet.specs.azure.region
+
+  tags = var.md_metadata.default_tags
 }
 
 resource "azurerm_redis_cache" "main" {
