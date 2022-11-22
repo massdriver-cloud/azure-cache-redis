8227753a754f1a76da4e4f5e0fb3cf813ce169a7
mclacore <michael@massdriver.cloud>
2022-11-22 10:24:16 -0700

    Replacing vnet with azure_virtual_network

--- src/main.tf
+++ src/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
-  location = var.vnet.specs.azure.region
+  location = var.azure_virtual_network.specs.azure.region
 
   tags = var.md_metadata.default_tags
 }
 
 resource "azurerm_redis_cache" "main" {
   name                = var.md_metadata.name_prefix
-  location            = var.vnet.specs.azure.region
+  location            = var.azure_virtual_network.specs.azure.region
   resource_group_name = azurerm_resource_group.main.name
 
   redis_version        = var.redis.redis_version
@@ -15,7 +15,7 @@ resource "azurerm_redis_cache" "main" {
   family               = "P"
   capacity             = var.redis.capacity
   sku_name             = "Premium"
-  subnet_id            = var.vnet.data.infrastructure.default_subnet_id
+  subnet_id            = var.azure_virtual_network.data.infrastructure.default_subnet_id
 
   identity {
     type = "SystemAssigned"
