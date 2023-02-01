0f747a7185bd85ed5b44aa18ca7fdcb6957490b3
mclacore <michael@massdriver.cloud>
2023-02-01 13:09:29 -0700

    Code for AOF data persistence support

--- src/main.tf
+++ src/main.tf
@@ -22,15 +22,19 @@ resource "azurerm_redis_cache" "main" {
   dynamic "redis_configuration" {
     for_each = var.redis.persistence ? [1] : []
     content {
-      rdb_backup_enabled = var.redis.persistence ? true : false
+      rdb_backup_enabled = var.redis.persistence_type == "RDB" ? true : false
       # Snapshot count seems to be a limitation, and can only be set to "1"
       # https://github.com/hashicorp/terraform-provider-azurerm/issues/19469
-      rdb_backup_max_snapshot_count = var.redis.persistence ? 1 : null
-      rdb_backup_frequency          = var.redis.persistence ? var.redis.persistence_type : null
+      rdb_backup_max_snapshot_count = var.redis.persistence_type == "RDB" ? 1 : null
+      rdb_backup_frequency          = var.redis.persistence_type == "RDB" ? var.redis.rdb_persistence : null
       # Right now the connection string is required to be set, even if we're using RBAC.
       # Submitted a GitHub issue in the provider for this:
       # https://github.com/hashicorp/terraform-provider-azurerm/issues/20223
-      rdb_storage_connection_string = var.redis.persistence ? azurerm_storage_account.rdb[0].primary_connection_string : null
+      rdb_storage_connection_string   = var.redis.persistence_type == "RDB" ? azurerm_storage_account.rdb[0].primary_connection_string : null
+      aof_backup_enabled              = var.redis.persistence_type == "AOF" ? true : false
+      aof_storage_connection_string_0 = var.redis.persistence_type == "AOF" ? azurerm_storage_account.aof0[0].primary_connection_string : null
+      # Per MSFT docs, if a second storage account is "removed", then the first storage account should also be used as the second
+      aof_storage_connection_string_1 = var.redis.persistence_type == "AOF" && var.redis.aof_persistence == true ? azurerm_storage_account.aof1[0].primary_connection_string : azurerm_storage_account.aof0[0].primary_connection_string
     }
   }
 
